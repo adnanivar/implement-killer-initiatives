5c1fec21b30f4ac079ff89ebafe172a412f2b481
Adnan Ivarsson <139991430+adnanivar@users.noreply.github.com>
2024-02-22 16:49:52 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+main();
+    const data = [];
+    const data = generateRandomData();
+    }
+        console.log("Random Number: " + item);
+
+function main() {
+}
+}
+    return data;
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+    data.forEach(item => {
+function generateRandomData() {
+
+    for (let i = 0; i < 10; i++) {
